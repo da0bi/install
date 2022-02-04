9aed768c1870c0c66eec56bd9721c363166400b7
da0bi <dab@geus.dk>
2022-02-04 12:08:50 +0100

    Reworked content after first installation.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -i
 
 # Installation script for my machines.
 # Installs following packages/programs
@@ -46,55 +46,63 @@
 # @ daniel binder
 # Copenhagen, 31.01.2022
 
-echo -e '\n...INSTALLATION SCRIPT STARTED.\n'
+printf '\n...INSTALLATION SCRIPT STARTED.\n'
 
-#########
-# .BASHRC edits - if necessary
-#########
-# export cargo path and add it to .bashrc
-echo -e '\n# add cargo path' >> ~/.bashrc
-echo 'export PATH=$PATH:$HOME/.cargo/bin' >> ~/.bashrc
-# export ipython path and add it to .bashrc
-echo -e '\n# add ipython path' >> ~/.bashrc
-echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
-# replace ls with exa in .bashrc
-echo -e '\n# replace ls with exa' >> ~/.bashrc
-echo 'alias ll='exa -al --color=always --group-directories-first'' >> ~/.bashrc
-echo -e '\n...BASHRC BLOCK FINISHED.\n'
+##########
+## .BASHRC edits - if necessary and .bashrc was not downloaded from github
+##########
+## export cargo path and add it to .bashrc
+#echo '# add cargo path' >> ~/.bashrc
+#echo 'export PATH=$PATH:$HOME/.cargo/bin' >> ~/.bashrc
+## export ipython path and add it to .bashrc
+#echo '# add ipython path' >> ~/.bashrc
+#echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
+## replace ls with exa in .bashrc
+#echo '# replace ls with exa' >> ~/.bashrc
+#echo "alias ll='exa -al --color=always --group-directories-first'" >> ~/.bashrc
+#source $HOME/.bashrc
+#
+#printf '\n...BASHRC BLOCK FINISHED.\n'
 
 #################################################
-############## START INSTALLATIONS ##############
+############# START INSTALLATIONS ###############
 #################################################
 # Update to latest package lists
 sudo apt update
+# cleaning
+sudo apt autoremove --purge
 
 #######
-# BASIC installations following 01_apt_install.txt
+# BASIC packages
 #######
 sudo apt install build-essential
 sudo apt install make
 sudo apt install git
 sudo apt install rclone
-echo -e '\n...BASIC BLOCK FINISHED.\n'
+# To be able to use add-apt-repository you may need to install software-properties-common
+sudo apt-get install software-properties-common
+printf '\n...BASIC BLOCK FINISHED.\n'
 
 ######
-# RUST installation following 02_rust.txt
+# RUST installation(s)
 ######
 # install rustup (https://rustup.rs)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# assure that ~/.cargo/bin is in your PATH
 # update cargo to latest version
+sudo apt install libssl-dev
 cargo install cargo --force
-# install packages
+# packages
 cargo install exa
 cargo install fd-find
 cargo install procs
 cargo install ripgrep
 # nvim tex lsp
 cargo install --git https://github.com/latex-lsp/texlab.git --locked
-echo -e '\n...RUST BLOCK FINISHED.\n'
+printf '\n...RUST BLOCK FINISHED.\n'
 
 #########
-# PYTHON3 packages installation following 03_python.txt
+# PYTHON3 packages
 #########
 sudo apt install python3-setuptools
 sudo apt install python3-pip
@@ -102,6 +110,7 @@ sudo apt install python3-dev
 sudo apt install jupyter-core
 # global installation of following python packages - commented out for pip-installations
 sudo apt-get install -y python3-numpy python3-numpy-dev python3-scipy python3-matplotlib
+# assure that ~/.local/bin is in your PATH
 # pip installations
 pip install ipython
 pip install Jupyter
@@ -112,54 +121,58 @@ pip install pandas
 pip install obspy
 # nvim python lsp
 pip install pyright
-echo -e '\n...PYTHON BLOCK FINISHED.\n'
-
-#########
-# ADD PPA for QGIS and NEOVIM and install, following 04_ppa.txt
-#########
-# To be able to use add-apt-repository you may need to install software-properties-common
-sudo apt-get install software-properties-common
-# Adding PPA to your system, update and install 
-sudo add-apt-repository ppa:ubuntugis/ppa
-sudo add-apt-repository ppa:neovim-ppa/stable
-# update
-sudo apt-get update
-# install
-sudo apt install qgis python-qgis qgis-plugin-grass
-sudo apt install neovim
-echo -e '\n...PPA BLOCK FINISHED.\n'
+printf '\n...PYTHON BLOCK FINISHED.\n'
 
 ########
-# NODEJS installation of latest stable version, following 05_nodejs.txt
+# NODEJS installation of latest stable version
 # following https://phoenixnap.com/kb/update-node-js-version
 #######
 sudo apt install nodejs
-npm cache clean -f
-npm install -g n
+sudo apt install npm
+sudo npm cache clean -f
+sudo npm install -g n
 sudo n stable
-# installations
+# packages
 # nvim bash lsp
 sudo npm i -g bash-language-server
 # tldr man pages
 sudo npm install -g tldr
-echo -e '\n...NODEJS BLOCK FINISHED.\n'
+printf '\n...NODEJS BLOCK FINISHED.\n'
 
 ########
-# NEOVIM installations, following 06_nvim.txt
-########
+# NEOVIM installation(s)
+#########
+# Adding PPA to your system
+sudo add-apt-repository ppa:neovim-ppa/stable
+# update
+sudo apt-get update
+# install
+sudo apt install neovim
 # packer package manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 # tex-, python-, and bash-lsp servers already installed within RUST, PYTHON, and NODEJS sections 
-# install Nerd Font to display lualine symbols - follow installation guidelines in 06_nvim.txt 
-# install gnome tweak with all extensions (last line of apt installs) to actually apply nerd font in the terminal
+# install Nerd Font to display e.g. lualine symbols - follow installation guidelines in nerdfont.txt 
+# installation of gnome tweak with all extensions (last line of apt installs) to be actually able to set
+# the nerd font for the terminal
 sudo add-apt-repository universe
 sudo apt install gnome-tweak-tool
 sudo apt install $(apt search gnome-shell-extension | grep ^gnome | cut -d / -f1)
-# follow 06_nvim.txt how to set nerd font in gnome tweak tool
-echo -e '\n...NEOVIM BLOCK FINISHED.\n'
+# follow nerdfont.txt how to set nerd font with the gnome tweak tool
+printf '\n...NEOVIM BLOCK FINISHED.\n'
+
+##########
+# QGIS installation
+#########
+# Adding PPA to your system 
+sudo add-apt-repository ppa:ubuntugis/ppa
+# update
+sudo apt-get update
+# install
+sudo apt install qgis python3-qgis qgis-plugin-grass
+printf '\n...QGIS BLOCK FINISHED.\n'
 
 #########
-# PSYSMON installation following 07_psysmon.txt
+# PSYSMON installation
 #########
 # install packages for pycairo
 sudo apt install libcairo2-dev pkg-config
@@ -167,29 +180,30 @@ sudo apt install libcairo2-dev pkg-config
 pip install numpy==1.21
 # install wxpython with correct wheel for compilation
 pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-20.04 wxPython
-# clone psysmon into ~/Programs/
-git clone https://github.com/stefanmaar/psysmon.git ~/Programs
+# clone psysmon into ~/Programs/psysmon
+git clone https://github.com/stefanmaar/psysmon.git ~/Programs/psysmon
 # add to python packages with 'pip install -e PFAD_ZUM_PSYSMON_ORDER_IN_DEM_SETUP.PY_LIEGT'
 pip install -e ~/Programs/psysmon
-# install maria db and follow 07_psysmon.txt how to set it up
+# install maria db and follow psysmon.txt how to set it up
 sudo apt install mariadb-server
-echo -e '\n...PSYSMON BLOCK FINISHED.\n'
+printf '\n...PSYSMON BLOCK FINISHED.\n'
 
 #########
-# PYROCKO installation following 08_pyrocko.txt 
+# PYROCKO installation following
+# https://github.com/pyrocko/pyrocko
 #########
 # clone github repository for the squirrel branch
-git clone -b squirrel https://git.pyrocko.org/pyrocko/pyrocko.git ~/Programs
+git clone -b squirrel https://git.pyrocko.org/pyrocko/pyrocko.git ~/Programs/pyrocko
 cd ~/Programs/pyrocko
 # automated installation of all required packages
 sudo python3 install_prerequisites.py
 # install with setup.py
 sudo python3 setup.py install
-echo -e '\n...PYROCKO BLOCK FINISHED.\n'
+printf '\n...PYROCKO BLOCK FINISHED.\n'
 
 ######
-# WINE installation following 09_wine.txt
-# and after https://news.itsfoss.com/wine-7-0-release/
+# WINE installation following
+# https://news.itsfoss.com/wine-7-0-release/
 ######
 # Enable support for 32-bit architecture
 sudo dpkg --add-architecture i386
@@ -199,17 +213,22 @@ wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo apt-add-repository "deb https://dl.winehq.org/wine-builds/ubuntu focal main"
 # update apt and install
 sudo apt update && sudo apt install --install-recommends winehq-stable
-# complete installation following 07_wine.txt
-echo -e '\n...WINE BLOCK FINISHED.\n'
+# complete installation following wine.txt
+printf '\n...WINE BLOCK FINISHED.\n'
 
 ########
-# ZOTERO and JURISM installation following 10_zotero.txt
+# ZOTERO and JURISM installation following 
+# https://github.com/retorquere/zotero-deb
 ########
 wget -qO- https://apt.retorque.re/file/zotero-apt/install.sh | sudo bash
 sudo apt update
 sudo apt install zotero
 sudo apt install jurism
-echo -e '\n...ZOTERO BLOCK FINISHED.\n'
+printf '\n...ZOTERO BLOCK FINISHED.\n'
+
+# Cleaning up
+sudo apt autoremove --purge 
+sudo apt clean
 
 echo '...INSTALLATION SCRIPT FINISHED.'
 
